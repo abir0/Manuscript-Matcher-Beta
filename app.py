959b5e166f3b25b4b43a0426c5556aefdae5c64c
abir0 <abir.spsc@gmail.com>
2023-08-12 00:48:58 +0600

    Update application to get predictions from Hugging Face Spaces API

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask, render_template, request
 
 
@@ -10,8 +11,23 @@ def home():
     if request.method == "POST":
         input_title = request.form.get('title')
         input_abstract = request.form.get('abstract')
-        print(input_title, input_abstract)
-    return render_template("index.html", title="Home")
+        output = predict_category(input_title, input_abstract)
+        print(output)
+        return render_template("index.html", title="Home", output_text=output['data'][0]['label'])
+    else:
+        return render_template("index.html", title="Home")
+
+
+def predict_category(title, abstract):
+    url = "https://abir0-manuscript-matcher.hf.space/run/predict"
+    text = title + ". \n" + abstract
+    data = {
+        "data": [
+            text
+        ],
+    }
+    response = requests.post(url, json=data)
+    return response.json()
 
 
 if __name__ == "__main__":
